1de8c6c96263a5a1874438168d6f1b1e64b7db79
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2021-04-04 20:16:59 +0700

    a

--- src/components/frame.js
+++ src/components/frame.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class Frame extends React.Component{
     render(){
         return (
-            <iframe src='http://pornhub.com/' style={{width: '100vw', height: '100vh'}}></iframe>
+            <iframe src='https://pornhub.com/' style={{width: '100vw', height: '100vh'}}></iframe>
         )
     }
 }
