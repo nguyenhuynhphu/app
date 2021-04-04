8a4b777f5e431a11853da1bd39ad29608a490321
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2021-04-04 20:07:57 +0700

    a

---
+++ src/components/frame.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Frame extends React.Component{
+    render(){
+        return (
+            <iframe src='https://kenh14.vn/' style={{width: '100vw', height: '100vh'}}></iframe>
+        )
+    }
+}
+export default Frame;
